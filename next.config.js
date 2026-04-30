60dc3938b62e3559cec61c3f698cbc4eb6c9534a
Shaheen <perwez58@gmail.com>
2026-04-30 12:46:33 +0530

    Configure GitHub Pages deployment for Next.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: isProd ? "/msneuro" : "",
+  assetPrefix: isProd ? "/msneuro/" : "",
+};
 
 module.exports = nextConfig;
